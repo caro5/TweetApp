098bbd56535b02c805d80a065d7fb37c7be9b206
Caroline Wong <carowong5@gmail.com>
2016-08-23 16:30:55 -0700

    In progress moving timeline to fragment

--- app/src/main/java/com/codepath/apps/tweetsapp/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/tweetsapp/TimelineActivity.java
@@ -4,134 +4,61 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Toast;
 
-import com.activeandroid.ActiveAndroid;
-import com.activeandroid.Configuration;
 import com.activeandroid.query.Select;
+import com.codepath.apps.tweetsapp.fragments.TweetsListFragment;
 import com.codepath.apps.tweetsapp.models.Tweet;
 import com.codepath.apps.tweetsapp.models.TweetModel;
-import com.codepath.apps.tweetsapp.models.UserModel;
-import com.codepath.apps.tweetsapp.utils.EndlessRecyclerViewScrollListener;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity implements ComposeFragment.ComposeFragmentListener {
-    @BindView (R.id.rvTweets) RecyclerView rvTweets;
     @BindView (R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeContainer;
-    @BindView(R.id.fabCreate) FloatingActionButton fabCreate;
 
+    private TweetsListFragment fragmentTweetsList;
     private TwitterClient client;
-    private ArrayList<Tweet> tweets;
-    private TweetsAdapter adapter;
-    LinearLayoutManager layoutManager;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        Configuration.Builder config = new Configuration.Builder(this);
-        config.addModelClasses(TweetModel.class, UserModel.class);
-        ActiveAndroid.initialize(config.create());
-
-        tweets = new ArrayList<>();
-        adapter = new TweetsAdapter(this, tweets);
-        rvTweets.setAdapter(adapter);
-        layoutManager = new LinearLayoutManager(this);
-        rvTweets.setLayoutManager(layoutManager);
+        if (savedInstanceState == null) {
+            fragmentTweetsList = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_timeline);
+        }
         client = TwitterApplication.getRestClient();
         if (!isNetworkAvailable() || !isOnline()) {
             Toast.makeText(this, "Unable to connect to the internet", Toast.LENGTH_LONG).show();
             List<TweetModel> queryResults = new Select().from(TweetModel.class)
                     .orderBy("created_at DESC").limit(40).execute();
             for (int i = 0; i < queryResults.size(); i++) {
-                tweets.add(Tweet.fromModel(queryResults.get(i)));
+                fragmentTweetsList.add(0, Tweet.fromModel(queryResults.get(i)));
             }
         } else {
-           populateTimeline(-1);
+            populateTimeline(-1);
         }
-        setupListeners();
     }
-
-    public void setupListeners() {
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                populateTimeline(-1);
-                swipeContainer.setRefreshing(false);
-            }
-        });
-        // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-
-        fabCreate.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (!isOnline() || !isNetworkAvailable()) {
-                    Toast.makeText(getApplicationContext(), "Connect to the internet before tweeting", Toast.LENGTH_LONG).show();
-                    return false;
-                }
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    composeMessage();
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-
-        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(layoutManager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount) {
-                if (!isOnline() || !isNetworkAvailable()) {
-                    Toast.makeText(getApplicationContext(), "Unable to connect to the internet", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                Tweet lastTweet = tweets.get(tweets.size() - 1);
-                long tweetId = lastTweet.getUid();
-                populateTimeline(tweetId);
-            }
-        });
-    }
-
     // Send API request to get timeline json
     // fill listview by creating tweet objects
     private void populateTimeline(long maxId) {
         client.getHomeTimeline(maxId, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
-                tweets.addAll(0, Tweet.fromJSONArray(json));
-                adapter.notifyItemRangeInserted(0, json.length());
-                rvTweets.scrollToPosition(0);
+                fragmentTweetsList.addAll(0, Tweet.fromJSONArray(json));
             }
 
             @Override
@@ -159,11 +86,8 @@ public class TimelineActivity extends AppCompatActivity implements ComposeFragme
 
     @Override
     public void onSuccessfulTweet(Tweet t) {
-        tweets.add(0, t);
-        adapter.notifyItemInserted(0);
-        rvTweets.scrollToPosition(0);
+        fragmentTweetsList.add(0, t);
     }
-
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
--- app/src/main/java/com/codepath/apps/tweetsapp/fragments/TweetsListFragment.java
+++ app/src/main/java/com/codepath/apps/tweetsapp/fragments/TweetsListFragment.java
@@ -2,27 +2,113 @@ package com.codepath.apps.tweetsapp.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codepath.apps.tweetsapp.R;
+import com.codepath.apps.tweetsapp.TweetsAdapter;
+import com.codepath.apps.tweetsapp.models.Tweet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
  * Created by cwong on 8/22/16.
  */
 public class TweetsListFragment extends Fragment {
+    @BindView(R.id.rvTweets) RecyclerView rvTweets;
+    @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeContainer;
+    @BindView(R.id.fabCreate) FloatingActionButton fabCreate;
+
+    private ArrayList<Tweet> tweets;
+    private TweetsAdapter adapter;
+    LinearLayoutManager layoutManager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_tweets_list, parent, false);
+        ButterKnife.bind(v);
         return v;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(getActivity(), tweets);
+        rvTweets.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(getActivity());
+        rvTweets.setLayoutManager(layoutManager);
+//        Configuration.Builder config = new Configuration.Builder(getActivity());
+//        config.addModelClasses(TweetModel.class, UserModel.class);
+//        ActiveAndroid.initialize(config.create());
+
+
+        // setupListeners();
     }
+//    public void setupListeners() {
+//        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//            @Override
+//            public void onRefresh() {
+//                populateTimeline(-1);
+//                swipeContainer.setRefreshing(false);
+//            }
+//        });
+//        // Configure the refreshing colors
+//        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+//                android.R.color.holo_green_light,
+//                android.R.color.holo_orange_light,
+//                android.R.color.holo_red_light);
+//
+//        fabCreate.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                if (!isOnline() || !isNetworkAvailable()) {
+//                    Toast.makeText(getActivity().getApplicationContext(), "Connect to the internet before tweeting", Toast.LENGTH_LONG).show();
+//                    return false;
+//                }
+//                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+//                    composeMessage();
+//                    return true;
+//                } else {
+//                    return false;
+//                }
+//            }
+//        });
+//
+//        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(layoutManager) {
+//            @Override
+//            public void onLoadMore(int page, int totalItemsCount) {
+//                if (!isOnline() || !isNetworkAvailable()) {
+//                    Toast.makeText(getActivity().getApplicationContext(), "Unable to connect to the internet", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//                Tweet lastTweet = tweets.get(tweets.size() - 1);
+//                long tweetId = lastTweet.getUid();
+//                populateTimeline(tweetId);
+//            }
+//        });
+//    }
+    public void addAll(int position, List<Tweet> tweetsList) {
+        tweets.addAll(position, tweetsList);
+        adapter.notifyItemRangeInserted(0, tweetsList.size());
+        rvTweets.scrollToPosition(0);
+    }
+    public void add(int position, Tweet tweet) {
+        tweets.add(position, tweet);
+        adapter.notifyItemInserted(0);
+        rvTweets.scrollToPosition(0);
+    }
+
 }
